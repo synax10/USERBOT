257aacfb3acee0ec9621f0a82b7b4a2a79106752
synax10 <150779327+synax10@users.noreply.github.com>
2023-11-14 17:01:15 +0530

    Update config.py

--- config.py
+++ config.py
@@ -13,12 +13,12 @@ SUDO_USERS = list(map(int, getenv("SUDO_USERS", "").split()))
 OWNER_ID = int(getenv("OWNER_ID"))
 MONGO_URL = getenv("MONGO_URL")
 BOT_TOKEN = getenv("BOT_TOKEN", "")
-ALIVE_PIC = getenv("ALIVE_PIC", 'https://telegra.ph/file/89f23433043a4bfa255c3.jpg')
+ALIVE_PIC = getenv("ALIVE_PIC", 'https://telegra.ph/file/a32783a91999e9774054a.jpg')
 ALIVE_TEXT = getenv("ALIVE_TEXT")
 PM_LOGGER = getenv("PM_LOGGER")
 LOG_GROUP = getenv("LOG_GROUP")
 GIT_TOKEN = getenv("GIT_TOKEN") #personal access token
-REPO_URL = getenv("REPO_URL", "https://github.com/itszshivam/OP-USERBOT2")
+REPO_URL = getenv("REPO_URL", "https://github.com/synax10/USERBOT")
 BRANCH = getenv("BRANCH", "master") #don't change
  
 STRING_SESSION1 = getenv("STRING_SESSION1", "")
